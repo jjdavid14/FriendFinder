cc1128f5ad65e354d1f1c53f3b6cd9d6f29ea6bb
JamieDavid <jjdavid14@live.com>
2018-02-09 22:57:14 -0700

    remove bad data

--- app/data/friends.js
+++ app/data/friends.js
@@ -31,26 +31,6 @@ var friendsArray = [{
     "name": "Hassan",
     "photo": "https://shop.wwe.com/on/demandware.static/-/Sites-wwe-us-navigation/default/dw2a6de264/images/superstar-thumb-300/john-cena-2017.jpg",
     "scores": ["5", "5", "5", "1", "3", "5", "5", "1", "1", "5"]
-}, {
-    "name": "name1",
-    "photo": "http://www.vetstreet.com/cats/",
-    "scores": ["4", "3", "2", "2", "1", "1", "3", "4", "3", "1"]
-}, {
-    "name": "name2",
-    "photo": "https://www.petfinder.com/cats/",
-    "scores": ["4", "3", "2", "2", "1", "1", "3", "4", "3", "1"]
-}, {
-    "name": "name3",
-    "photo": "http://markinternational.info/data/out/574/223950678-cats-pictures.jpg",
-    "scores": ["4", "3", "2", "2", "1", "1", "3", "4", "3", "1"]
-}, {
-    "name": "name5",
-    "photo": "http://markinternational.info/data/out/574/223950678-cats-pictures.jpg",
-    "scores": ["4", "3", "2", "2", "1", "1", "3", "4", "3", "1"]
-}, {
-    "name": "test",
-    "photo": "https://www.linkedin.com/in/eakta-pandey-57693828/",
-    "scores": ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
 }];
 
 // Note how we export the array. This makes it accessible to other files using require.
